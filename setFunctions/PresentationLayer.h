0851b202300a8f7c9dac5b501098e3a2e829da47
edkehayova18 <edkehayova18@codingburgas.bg>
2020-11-02 22:21:45 +0200

    Create presentationLayer.h

---
+++ setFunctions/PresentationLayer.h
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+void intersection()
+{
+	cout << endl;
+	cout << "   ***********************" << endl;
+	cout << " *          ****          *" << endl;
+	cout << "*          ******          *" << endl;
+	cout << "*          ******          *" << endl;
+	cout << "*          ******          *" << endl;
+	cout << " *          ****          *" << endl;
+	cout << "  ************************" << endl;
+	cout << endl;
+}
+
+void Union()
+{
+	cout << endl;
+	cout << "   **********  **********" << endl;
+	cout << " *********** ** ***********" << endl;
+	cout << "*********** **** ***********" << endl;
+	cout << "*********** **** ***********" << endl;
+	cout << "*********** **** ***********" << endl;
+	cout << " *********** ** ***********" << endl;
+	cout << "   **********  **********" << endl;
+	cout << endl;
+}
+
+void symetricalDifference()
+{
+	cout << endl;
+	cout << "   **********************" << endl;
+	cout << " ************  ************" << endl;
+	cout << "************    ************" << endl;
+	cout << "************    ************" << endl;
+	cout << "************    ************" << endl;
+	cout << " ************  ************" << endl;
+	cout << "   **********************" << endl;
+	cout << endl;
+}
+void differenceA()
+{
+	cout << endl;
+	cout << "   ************           *" << endl;
+	cout << " ************  *            *" << endl;
+	cout << "************    *            *" << endl;
+	cout << "************    *            *" << endl;
+	cout << "************    *            *" << endl;
+	cout << " ************  *            *" << endl;
+	cout << "   ************           *" << endl;
+	cout << endl;
+}
+void differenceB()
+{
+
+	cout << endl;
+	cout << "   *           ************" << endl;
+	cout << " *            *  ************" << endl;
+	cout << "*            *    ************" << endl;
+	cout << "*            *    ************" << endl;
+	cout << "*            *    ************" << endl;
+	cout << " *            *  ************" << endl;
+	cout << "   *           ************" << endl;
+	cout << endl;
+}
+void subset()
+{
+	cout << endl;
+	cout << "   ***********" << endl;
+	cout << " *        *    *" << endl;
+	cout << "*      *******  *" << endl;
+	cout << "*     ********* *" << endl;
+	cout << "*      *******  *" << endl;
+	cout << " *        *    *" << endl;
+	cout << "   ***********" << endl;
+	cout << endl;
+}
